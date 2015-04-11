be82e04a89c996a89b06993dd671a3f3a663e58d
stas <bakurow@list.ru>
2015-04-11 18:42:02 +0300

    init1

--- js/app/app.js
+++ js/app/app.js
@@ -13,19 +13,19 @@ angular.module('myApp', [
     ]).
     config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
-        /*//HTML5 URL
+        //HTML5 URL
         if(window.history && window.history.pushState){
             $locationProvider.html5Mode({
                 enabled: true,
                 requireBase: false
             });
-        }*/
+        }
 
         $routeProvider.
-            when('/addPost', {templateUrl: 'addPost/addPost.html',  controller: 'AddPostCtrl'}).
-            when('/welcome', {templateUrl: 'welcome/welcome.html',  controller: 'WelcomeCtrl'}).
-            when('/home',    {templateUrl: 'home/home.html',        controller: 'HomeCtrl'}).
-            when('/register',{templateUrl: 'register/register.html',controller: 'RegisterCtrl'}).
+            when('/addPost', {templateUrl: '/js/app/controllers/addPost/addPost.html',  controller: 'AddPostCtrl'}).
+            when('/welcome', {templateUrl: '/js/app/controllers/welcome/welcome.html',  controller: 'WelcomeCtrl'}).
+            when('/home',    {templateUrl: '/js/app/controllers/home/home.html',        controller: 'HomeCtrl'}).
+            when('/register',{templateUrl: '/js/app/controllers/register/register.html',controller: 'RegisterCtrl'}).
             otherwise(       {redirectTo:  '/home'});//вид по умолчанию
         /*$locationProvider.html5Mode(true);*/
     }]);
