d8f126ac5c0128d2ba3081d6e370ce1a7bbdc2aa
stas <bakurow@list.ru>
2015-04-27 08:04:41 +0300

    Blog init

--- js/app/controllers/addPost/addPost.js
+++ js/app/controllers/addPost/addPost.js
@@ -1,15 +1,20 @@
 'use strict';
 
 angular.module('myApp.addPost', [])
-    .controller('AddPostCtrl', ['$scope','$firebase','$menu', function($scope, $firebase, $menu) {
+    .controller('AddPostCtrl', ['$scope','$firebase','$menu','CommonProp','$location', function($scope, $firebase, $menu, CommonProp, $location) {
         $menu.setShowMenu(false);
         $scope.AddPost = function(){
             var title = $scope.article.title;
             var post = $scope.article.post;
-            var firebaseObj = new Firebase("https://db-myproject.firebaseio.com/Messages");
+            var firebaseObj = new Firebase("https://db-myproject.firebaseio.com/Articles");
             var fb = $firebase(firebaseObj);
-            fb.$push({ title: title,post: post}).then(function(ref) {
+            fb.$push({
+                title: title,
+                post: post,
+                emailId: CommonProp.getUser()
+            }).then(function(ref) {
                 console.log(ref);
+                $location.path('/welcome');
             }, function(error) {
                 console.log("Error:", error);
             });
--- js/app/controllers/welcome/welcome.js
+++ js/app/controllers/welcome/welcome.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('myApp.welcome', [])
-    .controller('WelcomeCtrl', ['$scope','CommonProp','$menu', function($scope,CommonProp, $menu) {
+    .controller('WelcomeCtrl', ['$menu','$scope','CommonProp', '$firebase', function($menu, $scope, CommonProp, $firebase) {
         $menu.setShowMenu(false);
         $scope.username = CommonProp.getUser();
+
+        var firebaseObj = new Firebase("https://db-myproject.firebaseio.com/Articles");
+        var sync = $firebase(firebaseObj);
+        var lst = sync.$asArray();
+        $scope.articles = lst;
     }]);
